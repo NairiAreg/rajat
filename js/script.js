5c9c73581f9d0595c659187dc67590d5fd44d0c9
Nairi Areg <hnairiareg@gmail.com>
2021-07-24 10:23:12 +0400

    V1

--- js/script.js
+++ js/script.js
@@ -1,6 +1,30 @@
 $(document).ready(function () {
-  $('.sizes .col-2').on('click',function(){
+  $('.sizes .col-2').on('click', function () {
     $('.sizes .col-2').removeClass('active');
     $(this).addClass("active");
   })
+
+  $('button.bg-main').on('click', function () {
+    if (!$('.sizes .col-2.active').length) {
+      $(".sizes").addClass('shake');
+      setTimeout(() => {
+        $(".sizes").removeClass('shake');
+      }, 500);
+    }
+  })
+
+
+  window.addEventListener('scroll', function () {
+    if (window.scrollY > 50) {
+      document.getElementById('navbar_top').classList.add('fixed-top');
+      // add padding top to show content behind navbar
+      navbar_height = document.querySelector('.navbar').offsetHeight;
+      document.body.style.paddingTop = navbar_height + 'px';
+    } else {
+      document.getElementById('navbar_top').classList.remove('fixed-top');
+      // remove padding top from body
+      document.body.style.paddingTop = '0';
+    }
+  });
+
 });
