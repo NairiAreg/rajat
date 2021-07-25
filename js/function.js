a0ef5f8e40f198d1491222d94cfef81ab44d85dc
Nairi Areg <hnairiareg@gmail.com>
2021-07-25 10:30:19 +0400

    V3

--- js/function.js
+++ js/function.js
@@ -10,19 +10,37 @@ $( document ).ready(function() {
 //       galleryMargin: 10,
 //       thumbMargin: 10,
 //       }; 
- 
+
+// Create a media condition that targets viewports at least 768px wide
+const mediaQuery = window.matchMedia('(min-width: 768px)')
+// Check if the media query is true
+if (mediaQuery.matches) {
+  // Then trigger an alert  
   $v_slider_options = {
-      gallery: true,
-      item: 1,
-      loop:true,
-      slideMargin: 0,
-      thumbItem: 5,
-      galleryMargin: 10,
-      thumbMargin: 10,
-      vertical: true,
-      verticalHeight: 545,
-      controls: false
+    gallery: true,
+    item: 1,
+    loop:true,
+    slideMargin: 0,
+    thumbItem: 5,
+    galleryMargin: 10,
+    thumbMargin: 10,
+    vertical: true,
+    verticalHeight: 545,
+    controls: false
   };
+} else {
+  $v_slider_options = {
+    gallery: false,
+    item: 1,
+    loop:true,
+    slideMargin: 0,
+    thumbItem: 5,
+    galleryMargin: 10,
+    thumbMargin: 10,
+    controls: true
+  };
+
+}
  
 //   h_slider = $('#lightSlider').lightSlider($h_slider_options);
   v_slider = $('#lightSliderVertical').lightSlider($v_slider_options);
